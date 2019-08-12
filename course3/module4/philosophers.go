46832638e7fad674d1a906a192dfca6e1479fc5f
Sam Thomas <arcoseal@gmail.com>
2019-08-12 22:25:15 +0100

    Finish philosophers/go

--- course3/module4/philosophers.go
+++ course3/module4/philosophers.go
@@ -4,101 +4,162 @@ import (
 	"fmt"
 	"sync"
 	"time"
+	"math/rand"
 )
 
-func mod_floor(x, n int) int { return ((x % n) + n) % n }
+// EatRequests are sent by Philopshers.Dine() to the Host
+// they contain the Philsopher's ID and a temporary channel for the Host to give (or deny) eating permission on
+type EatRequest struct {
+	id    int
+	ac chan bool
+}
 
+type Chopstick struct {
+	sync.Mutex
+}
+
+// a Philsopher has an ID, a left & right chopstick shared with its neighbours, some flags and a count of the servings it has eaten
 type Philosopher struct {
 	id             int
 	Lstick, Rstick *Chopstick
 	isFull         bool
+	isEating       bool
 	servings       int
 }
 
-func (p Philosopher) dine(req chan chan bool) {
+// Dine() will continuously try and eat until the Philsopher is full (has eaten maxServ times)
+// it does this by sending an EatRequest to the Host. if the Host grants permission it will initiate eating
+// if the Host denies permission it will try again
+// when it is done eating it notifies the Host via another channel
+func (p *Philosopher) Dine(rc chan EatRequest, dc chan bool, maxServ int) {
+	p.servings = 0
 	p.isFull = false
-	for p.servings < 3 {
-		fmt.Printf("%d requesting to eat\n", p.id)
-		approve := make(chan bool)
-		req <- approve
-		fmt.Printf("%d awaiting permission to eat\n", p.id)
-		_ := <-approve
-		fmt.Printf("%d received permission to eat\n", p.id)
-		p.eat()
+	for p.servings < maxServ {
+		fmt.Printf("%d: requesting to eat\n", p.id)
+		ac := make(chan bool) // "callback" channel for eating permission
+		req := EatRequest{p.id, ac}
+		rc <- req // request permission from host
+		fmt.Printf("%d: awaiting permission to eat\n", p.id)
+
+		v, ok := <-ac
+		if v && ok { // true was sent on ac -> permission was granted
+			fmt.Printf("%d: received permission to eat\n", p.id)
+			p.isEating = true
+			p.Eat() // get chopsticks & eat
+			p.isEating = false
+			p.servings++
+			fmt.Printf("%d: done eating, has had %d servings\n", p.id, p.servings)
+			dc <- true // notify host that someone is done eating
+		} else if !ok { // ac was closed -> permission was denied
+			fmt.Printf("%d: got rejected by host\n", p.id)
+		}
+		time.Sleep(100 * time.Millisecond) // limit looping speed
 	}
 	p.isFull = true
-	fmt.Printf("%d is full\n")
+	fmt.Printf("%d: is full\n", p.id)
 }
 
-func (p Philosopher) eat() {
+func (p *Philosopher) Eat() {
+	// acquire chopsticks
 	fmt.Printf("%d: Getting chopsticks...\n", p.id)
 	p.Lstick.Lock()
 	p.Rstick.Lock()
 
-	fmt.Printf("%d: Got chopsticks, eating...\n", p.id)
-	time.Sleep(1 * time.Second)
+	// eat dem noodles
+	fmt.Printf("Starting to eat: %d\n", p.id)
+	time.Sleep(time.Duration(rand.Intn(2000)) * time.Millisecond) // eat for up to 2 seconds
+	fmt.Printf("Finishing eating: %d\n", p.id)
 
-	fmt.Printf("%d: Finished eating, putting down chopsticks\n", p.id)
+	// release chopsticks
 	p.Lstick.Unlock()
 	p.Rstick.Unlock()
-
-	p.servings++
+	fmt.Printf("%d: Put down chopsticks\n", p.id)
 }
 
-type Chopstick struct{ sync.Mutex }
-
-func Waiter(philos *[]Philosopher, req chan chan bool, wg *sync.WaitGroup) {
-	var li, ri int
-	var eating int
-	var alldone bool
+func Host(philos []*Philosopher, req chan EatRequest, dc chan bool, wg *sync.WaitGroup) {
+	var eating int // number of currently eating philosophers
+	var alldone bool // will be true when all the philosophers are full
 
 	for !alldone {
 		select {
-		case i := <-req:
-			fmt.Printf("Waiter got request from %d\n", (*philos)[i].id)
-			ri = mod_floor(i-1, 5)
-			li = mod_floor(i+1, 5)
-			if !(*philos)[li].isEating && !(*philos)[ri].isEating && eating < 2 {
-				fmt.Printf("Waiter granting permission to %d to eat\n", (*philos)[i].id)
+		case er := <-req: // recieved EatRequest from philsopher
+			fmt.Printf("Host: got request from %d\n", er.id)
+			li, ri := lridx(er.id, len(philos)) // get neighbouring philsophers
+			if philos[li].isEating || philos[ri].isEating { // denial
+				fmt.Printf("Host: denying %d (neighbour is eating)\n", er.id)
+				close(er.ac)
+			} else if eating >= 2 {
+				fmt.Printf("Host: denying %d (2 people are already eating)\n", er.id)
+				close(er.ac)
+			} else { // grant
+				fmt.Printf("Host: granting permission to %d to eat\n", er.id)
 				eating++
-				*(*philos)[i].GrantEat()
-			} else {
-				fmt.Printf("%d denied by waiter\n", (*philos)[i].id)
+				er.ac <- true
 			}
-		case <-done:
-			fmt.Printf("Waiter heard someone was done eating\n")
+		
+		case <-dc: // received notification someone is done eating
 			eating--
-			// default:
-			// 	fmt.Printf("No outstanding requests to eat, waiter is checking if everyone is full...\n")
-			// 	for i, _ := range *philos {
-			// 		if !(*philos)[i].isFull {
-			// 			fmt.Printf("%d is still hungry\n", (*philos)[i].id)
-			// 			break
-			// 		}
-			// 	}
+			fmt.Printf("Host: heard someone was done eating: currently eating = %d\n", eating)
+		
+		default:
+			fmt.Printf("Host: No outstanding requests to eat, checking if everyone is full...\n")
+			alldone = allfull(philos)
 		}
+		time.Sleep(100 * time.Millisecond) // limit looping speed
 	}
+	fmt.Println("Host: everyone is full, ending dinner")
 	wg.Done()
 }
 
+
+// get indices to "left" & "right" of an index i, wrapping around at max length n
+func lridx(i, n int) (li, ri int) {
+	li = i
+	ri = i - 1
+	if ri < 0 {
+		ri = n - 1
+	}
+	return
+}
+
+// check if all philsophers are full of noodles
+func allfull(philos []*Philosopher) bool {
+	for i := range philos {
+		if !philos[i].isFull {
+			fmt.Printf("%d is still hungry\n", philos[i].id)
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
-	nPhilos := 5
-	chopsticks := make([]Chopstick, nPhilos)
-	philos := make([]Philosopher, nPhilos)
+	nPhilos := 5 // number of dining philosophers
+	maxServ := 3 // philosopher noodle capacity
 
-	req := make(chan int)
-	done := make(chan int)
 	var wg sync.WaitGroup
+	rc := make(chan EatRequest)
+	dc := make(chan bool)
 
-	for i, _ := range philos {
-		philos[i] = Philosopher{i, &chopsticks[mod_floor(i+1, 5)], &chopsticks[mod_floor(i-1, 5)], false, false, false, 0}
+	// slice of pointers to Chopsticks
+	chopsticks := make([]*Chopstick, nPhilos)
+	for i := range chopsticks {
+		chopsticks[i] = &Chopstick{}
+	}
+
+	// slice of pointers to Philsophers
+	philos := make([]*Philosopher, nPhilos)
+	for i := range philos {
+		li, ri := lridx(i, nPhilos)
+		philos[i] = &Philosopher{i, chopsticks[li], chopsticks[ri], false, false, 0}
 	}
 
 	wg.Add(1)
-	go Waiter(&philos, req, done, &wg)
-	for _, v := range philos {
-		go v.dine(req, done)
+	go Host(philos, rc, dc, &wg) // Host starts his shift
+	for _, v := range philos { // Philsophers sit down to dinner
+		go v.Dine(rc, dc, maxServ)
 	}
 	wg.Wait()
-	time.Sleep(30 * time.Second)
+
+	fmt.Println("Dinner is over")
 }
